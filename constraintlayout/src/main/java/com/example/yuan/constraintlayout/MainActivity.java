41544883ab01df247a3798a04c38522b9aca6dbf
water.yuan <water.yuan@cloudminds.com>
2018-11-01 13:32:50 +0800

    约束布局的使用

---
+++ constraintlayout/src/main/java/com/example/yuan/constraintlayout/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.yuan.constraintlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+// https://blog.csdn.net/guolin_blog/article/details/53122387
+// Android新特性介绍，ConstraintLayout完全解析
+// 约束布局
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
