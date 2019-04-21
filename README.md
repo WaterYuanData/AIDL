04d9f9ec058661c2d6c68e3bfefe1104234c986f
WaterYuan <water.yuan.data@gmail.com>
2019-04-22 00:20:48 +0800

    Retrofit的使用1

--- README.md
+++ README.md
@@ -8,3 +8,15 @@ okhttp3
 
 RxJava
 参考：https://www.jianshu.com/p/a406b94f3188
+
+结合 Retrofit 与RxJava 实现轮询需求
+https://www.jianshu.com/p/11b3ec672812
+步骤说明：
+添加依赖
+创建 接收服务器返回数据 的类
+创建 用于描述网络请求 的接口（区别于传统Retrofit形式）
+创建 Retrofit 实例
+创建 网络请求接口实例 并 配置网络请求参数（区别于传统Retrofit形式）
+发送网络请求（区别于传统Retrofit形式）
+发送网络请求
+对返回的数据进行处理
--- testrxjava/src/main/java/com/example/testrxjava/MainActivity.java
+++ testrxjava/src/main/java/com/example/testrxjava/MainActivity.java
@@ -4,11 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     //    private static final String TAG = "MainActivity";
@@ -19,9 +28,99 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        testRxJava();
+        testGet();
+
+    }
+
+    private void testGet() {
+        /*
+         * 步骤1：采用interval（）延迟发送
+         * 注：此处主要展示无限次轮询，若要实现有限次轮询，仅需将interval（）改成intervalRange（）即可
+         **/
+        Observable.interval(2, 1, TimeUnit.SECONDS)
+                // 参数说明：
+                // 参数1 = 第1次延迟时间；
+                // 参数2 = 间隔时间数字；
+                // 参数3 = 时间单位；
+                // 该例子发送的事件特点：延迟2s后发送事件，每隔1秒产生1个数字（从0开始递增1，无限个）
+
+                /*
+                 * 步骤2：每次发送数字前发送1次网络请求（doOnNext（）在执行Next事件前调用）
+                 * 即每隔1秒产生1个数字前，就发送1次网络请求，从而实现轮询需求
+                 **/
+                .doOnNext(new Consumer<Long>() {
+                    @Override
+                    public void accept(Long integer) throws Exception {
+                        Log.d(TAG, "第 " + integer + " 次轮询");
+
+                        /*
+                         * 步骤3：通过Retrofit发送网络请求
+                         **/
+                        // a. 创建Retrofit对象
+                        Retrofit retrofit = new Retrofit.Builder()
+                                .baseUrl("http://fy.iciba.com/") // 设置 网络请求 Url
+                                .addConverterFactory(GsonConverterFactory.create()) //设置使用Gson解析(记得加入依赖)
+                                .addCallAdapterFactory(RxJava2CallAdapterFactory.create()) // 支持RxJava
+                                .build();
+
+                        // b. 创建 网络请求接口 的实例
+                        GetRequest_Interface request = retrofit.create(GetRequest_Interface.class);
+
+                        // c. 采用Observable<...>形式 对 网络请求 进行封装
+                        Observable<Translation> observable = request.getCall();
+                        // d. 通过线程切换发送网络请求
+                        observable.subscribeOn(Schedulers.io())               // 切换到IO线程进行网络请求
+                                .observeOn(AndroidSchedulers.mainThread())  // 切换回到主线程 处理请求结果
+                                .subscribe(new Observer<Translation>() {
+                                    @Override
+                                    public void onSubscribe(Disposable d) {
+                                    }
+
+                                    @Override
+                                    public void onNext(Translation result) {
+                                        // e.接收服务器返回的数据
+                                        result.show();
+                                    }
+
+                                    @Override
+                                    public void onError(Throwable e) {
+                                        Log.d(TAG, "请求失败");
+                                    }
+
+                                    @Override
+                                    public void onComplete() {
+
+                                    }
+                                });
+
+                    }
+                }).subscribe(new Observer<Long>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Long value) {
+                Log.d(TAG, "onNext: 响应" + value);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "对Error事件作出响应");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "对Complete事件作出响应");
+            }
+        });
+    }
+
+    private void testRxJava() {
         // 步骤1：创建被观察者 Observable & 生产事件
         // 即 顾客入饭店 - 坐下餐桌 - 点菜
-
         //  1. 创建被观察者 Observable 对象
         Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
             // 2. 在复写的subscribe（）里定义需要发送的事件
@@ -40,7 +139,6 @@ public class MainActivity extends AppCompatActivity {
 
         // 步骤2：创建观察者 Observer 并 定义响应事件行为
         // 即 开厨房 - 确定对应菜式
-
         Observer<Integer> observer = new Observer<Integer>() {
             // 通过复写对应方法来 响应 被观察者
             @Override
@@ -65,10 +163,8 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-
         // 步骤3：通过订阅（subscribe）连接观察者和被观察者
         // 即 顾客找到服务员 - 点菜 - 服务员下单到厨房 - 厨房烹调
         observable.subscribe(observer);
-
     }
 }
